647ec82675a500825376ccdb44bfb93f95529a48
Nilesh1014 <kharadenilesh96@gmail.com>
2022-09-19 16:33:16 +0530

    First comitt....

---
+++ src/gitpackage/GitClass.java
@@ -0,0 +1,14 @@
+package gitpackage;
+
+public class GitClass {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("my name nilesh");
+		
+		System.out.println("my name nilesh kharade");
+		
+		
+	}
+
+}
